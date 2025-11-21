712ef53a3a72ee5ce26116ed0be361078f9b9892
Charles Severance <csev@umich.edu>
2025-11-21 14:41:58 -0500

    Updates.

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include, re_path
+from django.contrib.auth import views as auth_views
 from django.views.static import serve
 import os
 
